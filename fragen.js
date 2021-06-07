8499a99c7d7a8ab5d8a31a6d05d30707c7fb8632
Fabian Bär <baerfabian@gmx.de>
2021-06-07 17:18:42 +0200

    Create fragen.js

---
+++ fragen.js
@@ -0,0 +1,9 @@
+const QUESTIONS = [
+"Wer bist du eigentlich?",
+"Magst du Züge?",
+"Darf ich deine Telefonnummer haben?",
+"Was ist die beste Uni der Welt? (bitte nix falsches sagen!)",
+"Wo warst du in der Schule?",
+"Erzähl mal was über deine schlimmste Klausur!",
+"Mir fallen keine fragen mehr ein :("
+];
